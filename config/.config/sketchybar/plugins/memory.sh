ad337f3571985e8741d89f4684ca8c50f16cbf78
lichader <1524470+lichader@users.noreply.github.com>
2026-09-03 11:04:18 +1000

    fix(sketchybar): calculate memory usage accurately

--- config/.config/sketchybar/plugins/memory.sh
+++ config/.config/sketchybar/plugins/memory.sh
@@ -2,12 +2,17 @@
 
 MEMORY_PERCENT=$(vm_stat | awk -v total_memory="$(sysctl -n hw.memsize)" '
   /page size of/ { page_size = $8 }
-  /Pages active:/ { active = $3 }
+  /Anonymous pages:/ { anonymous = $3 }
+  /Pages purgeable:/ { purgeable = $3 }
   /Pages wired down:/ { wired = $4 }
+  /Pages occupied by compressor:/ { compressed = $5 }
   END {
-    gsub("\\.", "", active)
+    gsub("\\.", "", anonymous)
+    gsub("\\.", "", purgeable)
     gsub("\\.", "", wired)
-    printf "%.0f", ((active + wired) * page_size / total_memory) * 100
+    gsub("\\.", "", compressed)
+    used_pages = anonymous - purgeable + wired + compressed
+    printf "%.0f", (used_pages * page_size / total_memory) * 100
   }
 ')
 
